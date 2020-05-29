bb7465362fdce32a8d6e1f38666d19e04f810adc
kwitee <kwitee@gmail.com>
2020-05-29 17:57:48 +0200

    Exercise generator split.

---
+++ abc_exercise_randomizer/bar_generator.py
@@ -0,0 +1,84 @@
+import random
+from typing import List, Tuple
+
+from abc_exercise_randomizer.bar import Bar
+from abc_exercise_randomizer.bar_length import BarLength
+from abc_exercise_randomizer.note import Note
+from abc_exercise_randomizer.note_length import NoteLength
+from abc_exercise_randomizer.note_value import NoteValue
+
+
+class BarGenerator:
+    def __init__(self, note_distribution: List[Tuple[NoteValue, int]],
+                 length_distribution: List[Tuple[NoteLength, int]],
+                 tie_probability: float, bar_length: BarLength):
+        """
+        Creates bar generator instance.
+
+        Parameters:
+            note_distribution: Note probability distribution, higher value means greater probability.
+            length_distribution: Length probability distribution, higher value means greater probability.
+            tie_probability: Probability of ties between bars (must be in <0;1>).
+            bar_length: Defines how many quarter notes are in each bar (only meters with quarter notes are supported).
+        """
+
+        if tie_probability < 0 or tie_probability > 1:
+            raise ValueError
+
+        self.__possible_notes = self.__unroll_distribution(note_distribution)
+        self.__possible_lengths = self.__unroll_distribution(length_distribution)
+        self.__tie_probability = tie_probability
+        self.__bar_length = BarLength(bar_length).value
+
+    @staticmethod
+    def __unroll_distribution(distribution):
+        output = []
+
+        for weight in distribution:
+            for _ in range(weight[1]):
+                output.append(weight[0])
+
+        return output
+
+    def generate_bar(self, previous_bar: Bar = None, possible_tie: bool = False):
+        """
+        Generates random bar based on input parameters.
+
+        Parameters:
+            previous_bar: previously generated bar, None if this is the first bar.
+            possible_tie: controls if the ban can have a tie at the end.
+        Returns:
+            Random ABC exercise in string form.
+        """
+
+        length = 0
+        notes = []
+        note_value = str()
+
+        while length < self.__bar_length:
+            remaining_length = self.__bar_length - length
+            possible_length = [length for length in self.__possible_lengths if length.value.value <= remaining_length]
+            note_length = random.choice(possible_length)
+            note_value = random.choice(self.__get_possible_notes(length, previous_bar, note_value))
+
+            length = length + note_length.value.value
+
+            tie = possible_tie and length == self.__bar_length and self.__tie_probability > random.random()
+            notes.append(Note(note_value, note_length, tie))
+
+        return Bar(notes)
+
+    def __get_possible_notes(self, length: int, previous_bar: Bar, last_note: NoteValue):
+        possible_notes = self.__possible_notes
+
+        if length == 0 and previous_bar is not None:
+            last_note_in_previous_bar = previous_bar.get_notes[-1]
+
+            if last_note_in_previous_bar.get_tie:
+                possible_notes = [note for note in self.__possible_notes
+                                  if note == last_note_in_previous_bar.get_value]
+        else:
+            if last_note == NoteValue.rest:
+                possible_notes = [note for note in self.__possible_notes if note != NoteValue.rest]
+
+        return possible_notes
---
+++ abc_exercise_randomizer/score_generator.py
@@ -0,0 +1,90 @@
+import os
+
+from abc_exercise_randomizer.bar_generator import BarGenerator
+from abc_exercise_randomizer.bar_length import BarLength
+from abc_exercise_randomizer.score import Score
+
+
+class ScoreGenerator:
+
+    def __init__(self, bar_generator: BarGenerator, bar_length: BarLength, number_of_bars: int):
+        """
+        Creates score generator instance.
+
+        Parameters:
+            bar_generator: Bar generator used to generate score parts.
+            bar_length: Defines how many quarter notes are in each bar (only meters with quarter notes are supported).
+            number_of_bars: Number of bars to be generated (must be in <1;64>).
+        """
+
+        if number_of_bars < 1 or number_of_bars > 64:
+            raise ValueError
+
+        self.__bar_generator = bar_generator
+
+        self.__meter = f"{bar_length}/4"
+        self.__score_name = f"Random score"
+        self.__script_name = os.path.basename(__file__)
+        self.__default_note_length = "1/4"
+        self.__tempo = "1/4 = 50"
+        self.__key = "C"
+        self.__number_of_bars = number_of_bars
+
+    def __generate_header(self, number: int):
+        if number < 1:
+            raise ValueError
+
+        return f"X:{number}\n" \
+               f"T:{number} - {self.__score_name}\n" \
+               f"M:{self.__meter}\n" \
+               f"L:{self.__default_note_length}\n" \
+               f"Q:{self.__tempo}\n" \
+               f"K:{self.__key}\n" \
+               f"I:abc-creator {self.__script_name}\n"
+
+    def __generate_body(self):
+        bars = []
+        bar = None
+
+        for i in range(0, self.__number_of_bars):
+            possible_tie = i != self.__number_of_bars - 1
+            bar = self.__bar_generator.generate_bar(bar, possible_tie)
+            bars.append(bar)
+
+        return Score(bars)
+
+    def generate_score(self):
+        """
+        Generates random score based on input parameters.
+
+        Returns:
+            Random score in string form.
+        """
+
+        return self.__generate_score_with_number()
+
+    def __generate_score_with_number(self, number: int = 1):
+        return f"{self.__generate_header(number)}{self.__generate_body()}"
+
+    __SCORE_DELIMITER = "\n\n"
+
+    def generate_scores(self, count: int):
+        """
+        Generates number of random scores based on input parameters.
+
+        Parameters:
+            count: Number of scores. Can't be lower than one.
+        Returns:
+            Random score in string form.
+        """
+
+        if count < 1:
+            raise ValueError
+
+        scores = str()
+
+        for number in range(count):
+            delimiter = str() if number == 0 else self.__SCORE_DELIMITER
+            scores = scores + delimiter + self.__generate_score_with_number(number + 1)
+
+        return scores
