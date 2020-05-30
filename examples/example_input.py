ab56747a747a8d1721dca66de8f16c502af67652
kwitee <kwitee@gmail.com>
2020-05-30 11:12:26 +0200

    F3 note added. Example input updated.

--- examples/example_input.py
+++ examples/example_input.py
@@ -3,19 +3,19 @@ from abc_exercise_randomizer.note_value import NoteValue
 from abc_exercise_randomizer.note_length import NoteLength
 
 note_distribution = [
+    (NoteValue.f3, 3),
     (NoteValue.g3, 3),
     (NoteValue.a3, 2),
     (NoteValue.b3, 2),
-    (NoteValue.c4, 1),
-    (NoteValue.d4, 1),
-    (NoteValue.e4, 1),
-    (NoteValue.f4, 1),
-    (NoteValue.g4, 1),
+    (NoteValue.c4, 2),
+    (NoteValue.d4, 2),
+    (NoteValue.e4, 2),
+    (NoteValue.f4, 2),
+    (NoteValue.g4, 2),
     (NoteValue.rest, 1)
 ]
 
 length_distribution = [
-    (NoteLength.eighth, 1),
     (NoteLength.quarter, 6),
     (NoteLength.half, 4),
     (NoteLength.half_dot, 1),
