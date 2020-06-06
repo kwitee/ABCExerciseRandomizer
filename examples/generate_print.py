e1caaa26e810232b0c97dd6bc1d8642366ca4808
kwitee <kwitee@gmail.com>
2020-06-06 10:54:36 +0200

    Main methods added.

--- examples/generate_print.py
+++ examples/generate_print.py
@@ -1,7 +1,12 @@
 from abc_exercise_randomizer.exercise_generator import ExerciseGenerator
 from examples.example_input import *
 
-if __name__ == "__main__":
+
+def main():
     generator = ExerciseGenerator(note_distribution, length_distribution, tie_probability, syncopated, bar_length,
                                   number_of_bars)
     print(generator.generate_exercise())
+
+
+if __name__ == "__main__":
+    main()
--- examples/generate_to_file.py
+++ examples/generate_to_file.py
@@ -4,10 +4,13 @@ from abc_exercise_randomizer.exercise_generator import ExerciseGenerator
 from examples.example_input import *
 
 
-if __name__ == "__main__":
-    file_name = sys.argv[1]
+def main(file_name):
     generator = ExerciseGenerator(note_distribution, length_distribution, tie_probability, syncopated, bar_length,
                                   number_of_bars)
 
     with open(file_name, "w") as file:
         file.write(generator.generate_exercises(20))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
