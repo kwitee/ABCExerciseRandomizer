7066bed514b545e2b615d5803623349c27a73c7e
kwitee <kwitee@gmail.com>
2020-06-06 08:59:28 +0200

    If-main added.

--- examples/generate_print.py
+++ examples/generate_print.py
@@ -1,6 +1,7 @@
 from abc_exercise_randomizer.exercise_generator import ExerciseGenerator
 from examples.example_input import *
 
-generator = ExerciseGenerator(note_distribution, length_distribution, tie_probability, syncopated, bar_length,
-                              number_of_bars)
-print(generator.generate_exercise())
+if __name__ == "__main__":
+    generator = ExerciseGenerator(note_distribution, length_distribution, tie_probability, syncopated, bar_length,
+                                  number_of_bars)
+    print(generator.generate_exercise())
--- examples/generate_to_file.py
+++ examples/generate_to_file.py
@@ -4,9 +4,10 @@ from abc_exercise_randomizer.exercise_generator import ExerciseGenerator
 from examples.example_input import *
 
 
-file_name = sys.argv[1]
-generator = ExerciseGenerator(note_distribution, length_distribution, tie_probability, syncopated, bar_length,
-                              number_of_bars)
+if __name__ == "__main__":
+    file_name = sys.argv[1]
+    generator = ExerciseGenerator(note_distribution, length_distribution, tie_probability, syncopated, bar_length,
+                                  number_of_bars)
 
-with open(file_name, "w") as file:
-    file.write(generator.generate_exercises(20))
+    with open(file_name, "w") as file:
+        file.write(generator.generate_exercises(20))
