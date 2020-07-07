15d0a18413954f396c0fdfef00f4c63753282031
kwitee <kwitee@gmail.com>
2020-07-07 11:50:56 +0200

    Example input updated.

--- examples/example_input.py
+++ examples/example_input.py
@@ -4,20 +4,20 @@ from abc_exercise_randomizer.note_value import NoteValue
 from abc_exercise_randomizer.note_length import NoteLength
 
 note_distribution = [
-    (NoteValue.a2, 3),
-    (NoteValue.b2, 3),
+    (NoteValue.a2, 4),
+    (NoteValue.b2, 4),
     (NoteValue.c3, 3),
     (NoteValue.d3, 2),
-    (NoteValue.e3, 2),
-    (NoteValue.f3, 2),
-    (NoteValue.g3, 1),
-    (NoteValue.a3, 1),
-    (NoteValue.b3, 1),
-    (NoteValue.c4, 1),
-    (NoteValue.d4, 1),
+    (NoteValue.e3, 1),
+    (NoteValue.f3, 5),
+    (NoteValue.g3, 3),
+    (NoteValue.a3, 4),
+    (NoteValue.b3, 4),
+    (NoteValue.c4, 3),
+    (NoteValue.d4, 2),
     (NoteValue.e4, 1),
-    (NoteValue.f4, 1),
-    (NoteValue.g4, 1),
+    (NoteValue.f4, 5),
+    (NoteValue.g4, 3),
     (NoteValue.rest, 2)
 ]
 
@@ -30,6 +30,6 @@ length_distribution = [
 
 tie_probability = 0.05
 syncopated = False
-bar_length = BarLength.three
+bar_length = BarLength.four
 number_of_bars = 16
 key = Key.F
